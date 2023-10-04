ca3df0d922dbcf9890e74b1123d4251837b9dbf8
Mist <mist.zzh@gmail.com>
2023-10-04 21:10:07 +0800

    [Update] higlight when search

--- lua/options.lua
+++ lua/options.lua
@@ -19,6 +19,6 @@ vim.opt.showmode = false -- we are experienced, wo don't need the "-- INSERT --"
 
 -- Searching
 vim.opt.incsearch = true -- search as characters are entered
-vim.opt.hlsearch = false -- do not highlight matches
+vim.opt.hlsearch = true -- do not highlight matches
 vim.opt.ignorecase = true -- ignore case in searches by default
 vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
