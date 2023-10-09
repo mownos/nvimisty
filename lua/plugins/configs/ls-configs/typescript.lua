b7f10b36e801b31d8ba905842aa8be167a8bfea3
Mist <mist.zzh@gmail.com>
2023-10-09 21:03:53 +0800

    [Feat] setup ts & lua ls

--- lua/plugins/configs/ls-configs/typescript.lua
+++ lua/plugins/configs/ls-configs/typescript.lua
@@ -1,3 +1,3 @@
-local lspconfig = require("lspconfig")
-
-lspconfig.tsserver.setup({})
+return {
+	tsserver = {},
+}
--- lua/plugins/configs/lsp-config.lua
+++ lua/plugins/configs/lsp-config.lua
@@ -5,6 +5,8 @@ local snippet = require("luasnip.loaders.from_vscode")
 local import_util = require("utils.import")
 local lsp_attach_config = require("plugins.configs.lsp-attach-config")
 
+-- LSP kay mapping config
+vim.api.nvim_create_autocmd("LspAttach", lsp_attach_config)
 -- Snippet configuration
 snippet.lazy_load()
 
@@ -22,6 +24,4 @@ local ls_configs_path = package.searchpath(..., package.path):match(".+/") .. "l
 local language_configs = import_util.require_dir(ls_configs_path, "plugins.configs.ls-configs")
 
 lspconfig.lua_ls.setup(language_configs.lua)
-
--- LSP kay mapping config
-vim.api.nvim_create_autocmd("LspAttach", lsp_attach_config)
+lspconfig.tsserver.setup(language_configs.tsserver)
