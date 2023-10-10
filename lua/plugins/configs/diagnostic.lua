b6074d33dc1fcf911e987f852ac74b89557be36d
Mist <mist.zzh@gmail.com>
2023-10-10 18:18:49 +0800

    [Chore] format code style & change the prefix of virtual text

--- lua/plugins/configs/diagnostic.lua
+++ lua/plugins/configs/diagnostic.lua
@@ -1,22 +1,24 @@
 local sign = function(opts)
-  vim.fn.sign_define(opts.name, {
-    texthl = opts.name,
-    text = opts.text,
-    numhl = ''
-  })
+	vim.fn.sign_define(opts.name, {
+		texthl = opts.name,
+		text = opts.text,
+		numhl = "",
+	})
 end
 
-sign({name = 'DiagnosticSignError', text = '✘'})
-sign({name = 'DiagnosticSignWarn', text = '▲'})
-sign({name = 'DiagnosticSignHint', text = '⚑'})
-sign({name = 'DiagnosticSignInfo', text = ''})
+sign({ name = "DiagnosticSignError", text = "✘" })
+sign({ name = "DiagnosticSignWarn", text = "▲" })
+sign({ name = "DiagnosticSignHint", text = "⚑" })
+sign({ name = "DiagnosticSignInfo", text = "" })
 
 vim.diagnostic.config({
-  virtual_text = true,
-  severity_sort = true,
-  update_in_insert = true,
-  float = {
-    border = 'rounded',
-    source = 'always',
-  },
+	virtual_text = {
+		prefix = "",
+	},
+	severity_sort = true,
+	update_in_insert = true,
+	float = {
+		border = "rounded",
+		source = "always",
+	},
 })
