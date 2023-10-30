cfbf86c7332f37447aa8604ffa0338b69a283481
Mist <mist.zzh@gmail.com>
2023-10-30 23:23:46 +0800

    [Feat] turned on enhance_diff_hl

--- lua/plugins/configs/diffview-nvim.lua
+++ lua/plugins/configs/diffview-nvim.lua
@@ -1,6 +1,8 @@
 local actions = require("diffview.actions")
 
 require("diffview").setup({
+	enhanced_diff_hl = true,
+	diff_binaries = false,
 	file_panel = {
 		listing_style = "list", -- One of 'list' or 'tree'
 		win_config = {
