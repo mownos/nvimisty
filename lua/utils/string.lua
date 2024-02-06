10a55b1ad70f4dd40a3ee0d37430939d68ddd4d9
Mist <mist.zzh@gmail.com>
2024-02-06 21:23:53 +0800

    [Feat] add escape function for escape lua magic symbol for gsub

--- lua/utils/string.lua
+++ lua/utils/string.lua
@@ -4,4 +4,8 @@ function string_utils.char_at(content, index)
 	return string.sub(content, index, index)
 end
 
+function string_utils.escape(str)
+	return str:gsub("[%(%)%.%%%+%-%*%?%[%^%$%]]", "%%%1")
+end
+
 return string_utils
